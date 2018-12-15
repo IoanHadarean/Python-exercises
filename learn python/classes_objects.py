612c61a7f8b62dd029b469ed21abad87e885d99b
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-15 13:26:23 +0000

    Added solution to classes and objects exercise

--- learn python/classes_objects.py
+++ learn python/classes_objects.py
@@ -12,9 +12,21 @@ class Vehicle:
     color = ""
     value = 100.00
     def description(self):
-        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value) #.f means only 2 digits are printed after the point
         return desc_str
 # your code goes here
+
+car1 = Vehicle()
+car2 = Vehicle()
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60000.000
+car2.name = "Jump"
+car2.kind = "van"
+car2.color = "blue"
+car2.value = 10000.000
+
 
 # test code
 print(car1.description())
