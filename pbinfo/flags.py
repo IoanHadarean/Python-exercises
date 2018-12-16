0e109bd34c24ee77dd507435d93ea076ffae6426
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-16 23:55:42 +0000

    Added 352.py and string lists exercise plus solution

--- pbinfo/flags.py
+++ pbinfo/flags.py
@@ -1,7 +1,7 @@
 def show(sol, k, cul, nrsol):
-    print("solution number {0} ", nrsol)
-    for i in range(0, k):
-        print("{0} ", cul(sol[i]))
+    print("solution number {0} ".format(nrsol))
+    for i in range(0, k+1):
+        print("{0} ".format(cul(sol[i]))
             
 def validate(sol, k):
     v = 1
@@ -10,21 +10,20 @@ def validate(sol, k):
             v = 0
     if (k == 1 & (sol[k] == 2 | sol[k] == 5)):
         v = 0
-    if v == 1:
-        return v
+    return v
         
 def backtrackingi(cul):
     sol = []
     k = 0
     nrsol = 0
-    sol.append[-1]
+    sol[k] = -1
     while(k >= 0):
         if(sol[k] < 6):
             sol[k]+= 1
             if(validate(sol, k) == 1):
                 if(k == 2):
                     nrsol+= 1
-                    print(sol, k, cul, nrsol)
+                    show(sol, k, cul, nrsol)
                 else:
                     k+= 1
                     sol[k] = -1
---
+++ string_lists.py
@@ -0,0 +1,18 @@
+"""
+    Ask the user for a string and print out whether 
+    this string is a palindrome or not. (A palindrome 
+    is a string that reads the same forwards and backwards.)
+"""
+
+
+
+string = str(input("Tell me a string: "))
+
+reverse = string[::-1].upper()
+string = string.upper()
+
+if(reverse == string):
+    print("The string is palindrome")
+else:
+    print("The string is not palindrome")
+        
