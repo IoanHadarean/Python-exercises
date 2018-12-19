8661fb19bb5ba4a2e0080e9aac697c78361aaa12
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-19 12:08:25 +0000

    Updated 1582.py and added list comprehensions exercise

--- 1582.py
+++ 1582.py
@@ -1,5 +1,8 @@
+"""
+    Max digit sum and min digit sum for n numbers
 
 """
+
 n = int(input())
 
 sum = 0
@@ -17,30 +20,17 @@ for j in range(1, n):
     if(sum > max):
         max = sum
         a = m
+    else:
+        if(sum < min):
+            min = sum
+            b = m
     m = int(input())    
-print(m)
-"""
+print(a)
+print(b)
 
-n = int(input())
 
-sum = 0
-max = -1
-min = 1000
-
-m = int(input())
-aux = m
-for j in range(1, n):
-    while(aux != 0): 
-        digit = aux % 10
-        sum = sum + digit
-        aux = aux // 10
-    if(sum < min):
-        min = sum
-        a = m
-    m = int(input())
-print(m) 
-
-#n-am stiut sa le pun pe ambele in aceeasi solutie, desi dau bine, separat
+#am incercat sa le pun in niste variabile a = m, respectiv b =m(ca sa printez 
+#m), dar n-a mers, nici macar nu da bine :(, viata cruda :(
 
 
 
---
+++ list_comprehensions.py
@@ -0,0 +1,18 @@
+"""
+    Let’s say I give you a list saved in a variable: 
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write 
+    one line of Python that takes this list a and makes 
+    a new list that has only the even elements of this list in it.
+"""
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+b = []
+
+
+for elem in a:
+    if(elem % 2 == 0):
+        b.append(elem)
+        
+print(b)
